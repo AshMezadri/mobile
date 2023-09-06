c9459edc9eed0b54223ca7922ffff87ce1f7fd7e
Ash <ash.m10@aluno.ifsc.edu.br>
2023-09-06 12:00:27 -0300

    list

---
+++ List/app/src/main/java/com/example/list/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] nomes;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+
+        nomes = new String[]{"Pipoca", "Cenoura", "Doce"};
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, nomes);
+
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(getApplicationContext(),"Blablabla"+Integer.toString(i),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
